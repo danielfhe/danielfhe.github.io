b274f1f0deccde3daf8a262ac0202c6718607b31
Daniel He <danielfhe@gmail.com>
2023-10-03 11:55:40 -0700

    convert stat box to use react bootstrap form components - DH

--- stat-equivalence/src/StatBox.js
+++ stat-equivalence/src/StatBox.js
@@ -1,4 +1,5 @@
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 
 export default function StatBox({ label, stat, type, setStatValue }) {
     function onStatChange(e) {
@@ -6,9 +7,10 @@ export default function StatBox({ label, stat, type, setStatValue }) {
     }
 
     return (
-        <label>{label}
-            <input className="Stat-Box" type={type} min="0" defaultValue={stat} onChange={onStatChange}/>
-        </label>
+        <Form.Group>
+            <Form.Label>{label}</Form.Label>
+            <Form.Control type={type} min="0" defaultValue={stat} onChange={onStatChange}/>
+        </Form.Group>
     );
 }
 
