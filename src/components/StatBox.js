d264a1cc7e60ab95d502893765a5babdd2de1d00
Daniel He <danielfhe@gmail.com>
2023-11-17 14:39:40 -0800

    bring in ability to have tooltips - DH

--- src/components/StatBox.js
+++ src/components/StatBox.js
@@ -1,24 +1,54 @@
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import { BsInfoCircle } from 'react-icons/bs';
+import { Tooltip } from 'react-tooltip'
 
 export default function StatBox({ label, stat, type, setStatValue }) {
-    function onStatChange(e) {
-        setStatValue(e.target.value);
-    }
+  function onStatChange(e) {
+    setStatValue(e.target.value);
+  }
 
-    return (
-        <Form.Group>
-            <Form.Label>{label}</Form.Label>
-            <Form.Control type={type} min="0" defaultValue={stat} onChange={onStatChange}/>
-        </Form.Group>
-    );
+  return (
+    <Form.Group>
+      <Form.Label><LabelWithTooltip label={label}/></Form.Label>
+      <Form.Control type={type} min="0" defaultValue={stat} onChange={onStatChange}/>
+    </Form.Group>
+  );
 }
 
 export function HideableStatColumn({ label, stat, type, setStatValue, shouldShow }) {
-    return (
-        shouldShow && 
-            <Col md={3}>
-                <StatBox label={label} stat={stat} type={type} setStatValue={setStatValue}/>
-            </Col>
-    );
+  return (
+    shouldShow && 
+      <Col md={3}>
+        <StatBox label={label} stat={stat} type={type} setStatValue={setStatValue}/>
+      </Col>
+  );
+}
+
+function LabelWithTooltip({ label }) {
+  return (
+    <>
+      {label}&nbsp;
+      {
+        Object.keys(tooltips).includes(label) &&
+          <>
+            <Tooltip anchorSelect={'.' + tooltips[label].anchor}>{tooltips[label].text}</Tooltip>
+            <span className={tooltips[label].anchor}>
+              <BsInfoCircle/>
+            </span>
+          </>
+      }
+    </>
+  );
+}
+
+const tooltips = {
+  'Upper Damage Range': {
+    'text': 'Used to calculate total job ATT',
+    'anchor': 'tooltip-anchor-upper-damage-range'
+  },
+  // 'HP': {
+  //   'text': 'something for HP',
+  //   'anchor': 'tooltip-anchor-hp'
+  // }
 }
